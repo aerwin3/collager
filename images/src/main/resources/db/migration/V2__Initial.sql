5630a32401b4de3f876fdd1c8fe8d4f27e721936
aerwin3 <aerwin@mailgun.com>
2021-10-18 14:28:02 -0500

    adding flyways

---
+++ images/src/main/resources/db/migration/V2__Initial.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE IF NOT EXISTS images (
+    id uuid NOT NULL,
+    account character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    label character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    objects character varying(255) COLLATE pg_catalog."default",
+    url character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT images_pkey PRIMARY KEY (id)
+);
---
+++ images/src/main/resources/db/migration/V3__spring_session.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS public.spring_session
+(
+    primary_id character(36) COLLATE pg_catalog."default" NOT NULL,
+    session_id character(36) COLLATE pg_catalog."default" NOT NULL,
+    creation_time bigint NOT NULL,
+    last_access_time bigint NOT NULL,
+    max_inactive_interval integer NOT NULL,
+    expiry_time bigint NOT NULL,
+    principal_name character varying(100) COLLATE pg_catalog."default",
+    CONSTRAINT spring_session_pk PRIMARY KEY (primary_id)
+) TABLESPACE pg_default;
+
+CREATE UNIQUE INDEX spring_session_ix1
+    ON public.spring_session USING btree
+        (session_id COLLATE pg_catalog."default" ASC NULLS LAST)
+    TABLESPACE pg_default;
+
+CREATE INDEX spring_session_ix2
+    ON public.spring_session USING btree
+        (expiry_time ASC NULLS LAST)
+    TABLESPACE pg_default;
+
+CREATE INDEX spring_session_ix3
+    ON public.spring_session USING btree
+        (principal_name COLLATE pg_catalog."default" ASC NULLS LAST)
+    TABLESPACE pg_default;
+
